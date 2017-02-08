57179dbbf19ae4fd7847d37db89552631d35cfff
Giovanni Lovato <giovanni.lovato@saiv.it>
2017-02-08 16:34:35 +0100

    chore(project): port to maven

---
+++ fontawesomelabel-addon/src/test/java/com/kbdunn/vaadin/addons/fontawesome/FontAwesomeLabelTest.java
@@ -0,0 +1,13 @@
+package com.kbdunn.vaadin.addons.fontawesome;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+// JUnit tests here
+public class FontAwesomeLabelTest {
+
+	@Test
+	public void thisAlwaysPasses() {
+		Assert.assertEquals(true, true);
+	}
+}
---
+++ fontawesomelabel-demo/src/main/java/com/kbdunn/vaadin/addons/fontawesome/demo/DemoUI.java
@@ -0,0 +1,41 @@
+package com.kbdunn.vaadin.addons.fontawesome.demo;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.kbdunn.vaadin.addons.fontawesome.FontAwesome;
+import com.kbdunn.vaadin.addons.fontawesome.FontAwesomeLabel;
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+@Theme("demo")
+@Title("FontAwesomeLabel Add-on Demo")
+@SuppressWarnings("serial")
+public class DemoUI extends UI {
+
+    @WebServlet(value = "/*", asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = DemoUI.class)
+    public static class Servlet extends VaadinServlet {
+    }
+
+    @Override
+    protected void init(VaadinRequest request) {
+        // Initialize our new UI component
+        final FontAwesomeLabel component = FontAwesome.FONT_AWESOME.getLabel()
+                                                                   .setSize5x();
+        component.setSizeUndefined();
+
+        // Show it in the middle of the screen
+        final VerticalLayout layout = new VerticalLayout();
+        layout.setStyleName("demoContentLayout");
+        layout.setSizeFull();
+        layout.addComponent(component);
+        layout.setComponentAlignment(component, Alignment.MIDDLE_CENTER);
+        setContent(layout);
+    }
+}
